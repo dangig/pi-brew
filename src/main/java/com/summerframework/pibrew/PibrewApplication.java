ec9ecd46c8f5fcfef3c70ed3394878807b832e05
Daniel Giguere <github@danielgiguere.com>
2016-03-09 14:37:01 -0500

    pi-brew Initial commit

---
+++ src/main/java/com/summerframework/pibrew/PibrewApplication.java
@@ -0,0 +1,12 @@
+package com.summerframework.pibrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibrewApplication.class, args);
+	}
+}
